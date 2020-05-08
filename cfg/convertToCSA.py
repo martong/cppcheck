69e66bc3686f5e27eaec225e2c5b6d1488ec2dfa
Gabor Marton <gabor.marton@ericsson.com>
2020-05-08 16:12:36 +0200

    Try to parse signature

--- cfg/convertToCSA.py
+++ cfg/convertToCSA.py
@@ -2,75 +2,48 @@
 import subprocess
 import sys
 import xml.etree.ElementTree as ET
+import re
 from io import StringIO
 
 
-blacklist = [
-    'strchr',
-    'ctime',
-    'fflush',
-    'fputc',
-    'fputs',
-    'getenv',
-    'printf',
-    'setlocale',
-    'signal',
-    'strchr',
-    'strerror',
-    'strrchr',
-    'system',
-    'time',
-    'tolower',
-    'toupper',
-    'va_copy',
-    'va_end',
-    'va_start',
-    'wctomb',
-    'memchr'
-]
-
-whitelist = [
-'select',
-'FD_ZERO',
-'close',
-'htons',
-'socket',
-'setsockopt',
-'FD_ISSET',
-'FD_SET',
-'connect',
-'stat',
-'send',
-'getsockname',
-'poll',
-'ntohs',
-'accept',
-'fstat',
-'unlink',
-'getaddrinfo',
-'strtok_r',
-'alarm',
-'sendto',
-'gettimeofday',
-'listen',
-'ioctl',
-'clock_gettime',
-'lseek',
-'FD_CLR',
-'strtok',
-'socketpair',
-'recvfrom',
-'sigaction',
-'setrlimit',
-'getrlimit',
-'getpid',
-'gethostname',
-'gethostbyname',
+Builtin = [
+    'int',
+    'size_t',
+    'char',
+    'unsigned',
+    'signed',
+    'long',
+    'short',
+    'void',
+    'float',
+    'double',
+    'const',
+    'volatile',
+    'restrict'
 ]
 
 
 def decr(nr):
     return str(int(nr) - 1)
+
+
+def transferType(ty):
+    ty = list(filter(None, ty.split(' ')))
+    res = ''
+
+    for subty in ty:
+        if subty != '*' and subty not in Builtin:
+            return 'Irrelevant'
+
+    for subty in ty:
+        if subty == '*':
+            res += 'Ptr'
+        elif subty == "size_t":
+            res += 'Size'
+        elif subty in Builtin:
+            res += subty.capitalize()
+    res += 'Ty'
+    return res
 
 
 root = ET.parse(sys.argv[1]).getroot()
@@ -86,12 +59,6 @@ for function in root.findall('function'):
         if name.endswith("_l"):
             continue
 
-        # if name in blacklist:
-            # continue
-
-        # if name not in whitelist:
-            # continue
-
         if name.startswith("std::"):
             continue
 
@@ -101,7 +68,7 @@ for function in root.findall('function'):
         # Get the signature that is in a comment. Unfortunately comments are
         # skipped from the XML during read.
         sign = subprocess.run(
-            ['/bin/grep', str(name) + '(', sys.argv[1]],
+            ['/bin/grep', '-P', '\\b' + str(name) + '\\(.*\\)\s*;', sys.argv[1]],
             stdout=subprocess.PIPE)
         sign = sign.stdout.decode('utf-8')
         sign = sign.replace(
@@ -111,11 +78,41 @@ for function in root.findall('function'):
             '').replace(
             '\n',
             '').strip()
-        print("// " + sign)
 
-        print("addToFunctionSummaryMap(\"" + str(name) +
-              # "\",\nSummary(ArgTypes{}, RetType{}, " + pureness + ")")
-              "\",\nSummary(" + pureness + ")")
+        Ret = ''
+        Args = []
+        if (len(sign)):
+            print("// " + sign)
+            # Strip of names of args.
+            sign = re.sub(r'\w+,', r',', sign)
+            sign = re.sub(r'\w+\)', r')', sign)
+            # Strip off name of fun.
+            sign = re.sub(r'\w+\(', r'(', sign)
+            # pointers
+            sign = re.sub(r'(\S+)\*', r'\1 *', sign)
+            # sign = re.sub(r'\*(\w+)', r'* \1', sign)
+            # print("// " + sign)
+            # Get the return type.
+            Ret = sign.split('(')[0]
+            # print("Ret: ", Ret)
+            Ret = transferType(Ret)
+            # print("Ret: ", Ret)
+            Args = sign[sign.find('(') + 1: sign.rfind(')')]
+            Args = Args.split(',')
+            # print("Args: ", Args)
+            Args = [transferType(i) for i in Args]
+            # print("Args: ", Args)
+
+        # sys.stdout = old_stdout
+        # print(mystdout.getvalue())
+        # continue
+
+        print('addToFunctionSummaryMap("{}"\nSummary(ArgTypes{{{}}}, RetType{{{}}}, {})'.
+              format(str(name), ','.join(Args), Ret, pureness))
+
+        # print("addToFunctionSummaryMap(\"" + str(name) +
+              # # "\",\nSummary(ArgTypes{}, RetType{}, " + pureness + ")")
+              # "\",\nSummary(" + pureness + ")")
 
         returnValue = function.find('returnValue')
         returnValueConstraint = None
